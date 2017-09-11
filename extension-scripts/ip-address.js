484793251ea50da0b9ca406566b02931d493c93a
Ying Zhong <0x00eeee@gmail.com>
2017-09-11 09:54:44 -0500

    Update ip-address.js

--- extension-scripts/ip-address.js
+++ extension-scripts/ip-address.js
@@ -1,23 +1,18 @@
+var options = [
+  { name: "ip.cn", url: "http://ip.cn" },
+  { name: "ipify.org", url: "https://api.ipify.org" }
+]
+
 $ui.menu({
-  items: ["ip.cn", "ipify.org"],
+  items: options.map(function(item) { return item.name }),
   handler: function(title, idx) {
-    if (idx===0) {
-      $http.post({
-        url: "http://ip.cn",
-        header: {
-          "User-Agent": "curl/1.0"
-        },
-        handler: function(resp) {
-          $ui.alert(resp.data)
-        }
-      })
-    } else {
-      $http.get({
-        url: "https://api.ipify.org",
-        handler: function(resp) {
-          $ui.alert(resp.data)
-        }
-      })
-    }
+    var option = options[idx]
+    $http.get({
+      url: option.url,
+      header: { "User-Agent": "curl/1.0" },
+      handler: function(resp) {
+        $ui.alert(resp.data)
+      }
+    })
   }
 })
