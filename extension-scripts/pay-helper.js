24bea814af5e773d9fc00a1515b630fc1d2378be
Ying Zhong <0x00eeee@gmail.com>
2017-09-12 23:11:00 -0500

    Update pay-helper.js

--- extension-scripts/pay-helper.js
+++ extension-scripts/pay-helper.js
@@ -1,11 +1,11 @@
 var data = {
-  title: [
-    "Apple Pay",
+  titles: [
+    " Pay",
     "支付宝付款码",
     "支付宝扫码",
     "微信扫一扫",
   ],
-  url: [
+  urls: [
     "shoebox://",
     "alipayqr://platformapi/startapp?saId=20000056",
     "alipayqr://platformapi/startapp?saId=10000007",
@@ -13,20 +13,9 @@ var data = {
   ]
 }
 
-$ui.render({
-  props: {
-    title: "支付方式"
-  },
-  views: [{
-    type: "list",
-    props: {
-      data: data.title
-    },
-    layout: $layout.fill,
-    events: {
-      didSelect: function(tableView, indexPath, title) {
-        $app.openURL(data.url[indexPath.row])
-      }
-    }
-  }]
+$ui.menu({
+  items: data.titles,
+  handler: function(title, idx) {
+    $app.openURL(data.urls[idx])
+  }
 })
