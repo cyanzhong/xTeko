0cae6c7b6c5a67c09aa8f8638199e7ac39f33259
mysteriouss <mysteriouss@126.com>
2017-09-29 09:24:55 +0800

    Remove invalid method

--- extension-scripts/dota-pccold.js
+++ extension-scripts/dota-pccold.js
@@ -1,18 +1,4 @@
-function randomWord(randomFlag, min, max) {
-  var str = ""
-  var range = min
-  var arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  if (randomFlag) {
-    range = Math.round(Math.random() * (max - min)) + min
-  }
-  for (var i = 0; i < range; i++) {
-    var pos = Math.round(Math.random() * (arr.length - 1))
-    str += arr[pos]
-  }
-  return str
-}
 var roomId = '20360'
-var did = randomWord(false, 32).toUpperCase()
 var tt = (new Date()).getTime().toString().substr(0, 10)
 var apikey = 'Y237pxTx2In5ayGz'
 var authstr = "room/" + roomId + "?aid=androidhd1&cdn=ws&client_sys=android&time=" + tt
