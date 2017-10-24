56898855ecaafa1d68e673ec94da4fca0bcdff0b
cyan <0x00eeee@gmail.com>
2017-10-24 12:47:14 -0700

    Update clipboard-expiration.js

--- extension-demos/clipboard-expiration.js
+++ extension-demos/clipboard-expiration.js
@@ -1,8 +1,4 @@
-var date = new Date()
-date.setSeconds(date.getSeconds() + 20)
-
-$clipboard.set({
-  value: "Temporary text",
-  type: "public.plain-text",
-  expirationDate: date
+$clipboard.copy({
+  text: "Temporary text",
+  ttl: 20
 })
