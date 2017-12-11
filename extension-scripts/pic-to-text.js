9bf622e28d7e6fe8ae17c1bea76e65fb94779056
Leon Yeu <yeuleh@gmail.com>
2017-12-11 22:12:08 +0800

    create orc script

--- extension-scripts/pic-to-text.js
+++ extension-scripts/pic-to-text.js
@@ -1,19 +1,46 @@
-const ocrEngines = [
-  {
-    name: "百度 OCR",
-    api_url: "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic",
-    api_key: "",
-    sec_key: "",
-    access_token: ""
+const baidu_api_auth = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='
+const baidu_ocr_api = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token='
+const baidu_ocr_api_key = ''
+const baidu_ocr_sec_key = ''
+var baidu_access_token = ''
+
+function requestBaiduOcr(data, imageOrUrl) {
+  $http.post({
+    url: baidu_api_auth + baidu_ocr_api_key + '&client_secret=' + baidu_ocr_sec_key,
+    handler: function(resp) {
+      baidu_access_token = resp.data.access_token
+      requestBaiduOcrApi(data, imageOrUrl)
+    }
+  })
+}
+
+function requestBaiduOcrApi(data, imageOrUrl) {
+  if (!baidu_access_token) {
+    return
   }
-]
+  var requestData
+  if (imageOrUrl) {
+    requestData = {'image': $text.URLEncode($text.base64Encode(data))}
+  } else {
+    requestData = {'url': data}
+  }
+  $http.post({
+    url: baidu_ocr_api + baidu_access_token,
+    header: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: requestData,
+    handler: function(resp) {
+      resultData = resp.data
+      $ui.alert(resultData)
+    }
+  }) 
+}
 
 function pickImage() {
   $photo.pick({
     handler: function(resp) {
       var image = resp.image
       if (image) {
-        ocrImage(image.jpg(1.0))
+        requestBaiduOcr(image.jpg(1.0), true)
       } else {
         $ui.loading(false)
       }
@@ -21,52 +48,23 @@ function pickImage() {
   })
 }
 
-function ocrImage(data) {
-  $ui.loading(true)
-  var encodedData = encodeURI(btoa(data))
-  showOcrEngines(encodedData)
-  
-}
-
-function showOcrEngines(imageData) {
-  $ui.menu({
-    items: ocrEngines.map(function(item) { return item.name }),
-    handler: function(title, idx) {
-      var api_url = ocrEngines[idx].api_url
-      $http.request({
-        header: { "Content-Type":"application/x-www-form-urlencoded" },
-        body: imageData,
-        handler: function(resp) {
-          $ui.alert(resp.data)
-        }
-      })
-    }
-  })
-}
-
 var inputData = $context.data
 //var inputLink = $context.link
 var clipData = $clipboard.image
-//var clipLink = $clipboard.link
+var clipLink = $clipboard.link
 
-if (inputData) {
-  ocrImage(inputData)
-//} else if (inputLink) {
-//  showEngines(inputLink)
-} else if (clipData) {
+if (clipLink) {
+  $ui.menu({
+    items: ["搜索剪贴板链接", "从相册选择图片"],
+    handler: function(title, idx) {
+      idx == 0 ? requestBaiduOcr(clipLink, false) : pickImage()
+    }
+  })
+} else {
   $ui.menu({
     items: ["搜索剪贴板图片", "从相册选择图片"],
     handler: function(title, idx) {
-      idx == 0 ? searchImage(clipData) : pickImage()
+      idx == 0 ? requestBaiduOcr(clipData, true) : pickImage()
     }
   })
-// } else if (clipLink) {
-//   $ui.menu({
-//     items: ["搜索剪贴板链接", "从相册选择图片"],
-//     handler: function(title, idx) {
-//       idx == 0 ? showEngines(clipLink) : pickImage()
-//     }
-//   })
-} else {
-  pickImage()
 }
