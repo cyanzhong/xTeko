a01aeda6eef1ada5932d7173a851485068fa949a
cyan <0x00eeee@gmail.com>
2017-12-18 00:02:19 +0800

    Improve inbox

--- extension-demos/sync-inbox.js
+++ extension-demos/sync-inbox.js
@@ -1,22 +1,41 @@
-var scheme = "inbox://";
-var files = $file.list(scheme)
-
 $app.strings = {
   "en": {
+    "MAIN_TITLE": "Installer",
+    "EMPTY_TIPS": "Please import script use AirDrop or File Sharing",
     "IMPORT_DELETE": "Import & Delete",
     "DELETE_DIRECTLY": "Delete Directly"
   },
   "zh-Hans": {
+    "MAIN_TITLE": "脚本安装器",
+    "EMPTY_TIPS": "请通过 AirDrop 或文件共享导入脚本",
     "IMPORT_DELETE": "导入后删除",
     "DELETE_DIRECTLY": "直接删除"
   }
 }
 
+var file = $context.data
+
+if (file && file.fileName) {
+  install(file.fileName, file)
+  return
+}
+
+function install(name, data) {
+  $addin.save({name: name, data: data})
+}
+
+var scheme = "inbox://";
+var files = $file.list(scheme) || []
+
 $ui.render({
-  props: {title: "Inbox"},
+  props: {
+    title: $l10n("MAIN_TITLE")
+  },
   views: [{
     type: "list",
-    props: {data: files},
+    props: {
+      data: files
+    },
     layout: $layout.fill,
     events: {
       didSelect: function(sender, indexPath) {
@@ -30,7 +49,7 @@ $ui.render({
           items: [$l10n("IMPORT_DELETE"), $l10n("DELETE_DIRECTLY")],
           handler: function(title, idx) {
             if (idx == 0) {
-              $addin.save({name: name, data: $file.read(path)})
+              install(name, $file.read(path))
               _delete()
             } else if (idx == 1) {
               _delete()
@@ -38,6 +57,17 @@ $ui.render({
           }
         })
       }
+    }
+  }, {
+    type: "label",
+    props: {
+      hidden: files.length > 0,
+      text: $l10n("EMPTY_TIPS"),
+      lines: 0,
+      align: $align.center
+    },
+    layout: function(make, view) {
+      make.left.right.top.bottom.inset(20)
     }
   }]
 })
