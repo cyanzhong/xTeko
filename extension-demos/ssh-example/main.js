9f7dd6483c366144f0d55a8ced3787ec693cb896
cyan <0x00eeee@gmail.com>
2018-04-06 12:33:49 +0800

    Add ssh-example

---
+++ extension-demos/ssh-example/main.js
@@ -0,0 +1,24 @@
+const public_key = $file.read("resources/id_rsa.pub").string
+const private_key = $file.read("resources/id_rsa").string
+
+$ssh.connect({
+  host: "",
+  port: 22,
+  username: "",
+  public_key: public_key,
+  private_key: private_key,
+  script: "ls -l /var/lib/",
+  handler: function(session, response) {
+    console.log("connect: " + session.connected)
+    console.log("authorized: " + session.authorized)
+    console.log("response: " + response)
+    console.log("port: " + session.port)
+    session.channel.upload({
+      path: "resources/notes.md",
+      dest: "~/notes.md",
+      handler: function(success) {
+        console.log("success: " + success)
+      }
+    })
+  }
+})
