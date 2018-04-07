960191a5137a8c92f3f5c50b233febb6aebc2c0b
cyan <0x00eeee@gmail.com>
2018-04-07 11:46:08 +0800

    Update year-progress.js

--- extension-scripts/year-progress.js
+++ extension-scripts/year-progress.js
@@ -19,48 +19,77 @@ var percentage = now.getDaysOfYear() * 1.0 / (now.isLeapYear ? 366 : 365)
 $ui.render({
   views: [
     {
-    type: "view",
-    layout: function(make, view) {
-      make.top.bottom.equalTo(0)
-      make.left.right.inset(20)
-    },
-    views: [
-      {
-        type: "label",
-        props: {
-          text: (percentage * 100).toFixed(2) + "%",
-          font: $font("bold", 24)
-        },
-        layout: function(make, view) {
-          make.right.equalTo(0)
-          make.centerY.equalTo(view.super)
-        }
+      type: "view",
+      layout: function(make, view) {
+        make.height.equalTo(32)
+        make.left.top.right.inset(20)
       },
-      {
-        type: "view",
-        props: {
-          bgcolor: $rgba(21, 126, 251, 0.2),
-          radius: 8
-        },
-        layout: function(make, view) {
-          make.left.equalTo(0)
-          make.height.equalTo(32)
-          make.centerY.equalTo(view.super)
-          make.right.equalTo($("label").left).offset(-10)
-        },
-        views: [
-          {
-            type: "view",
-            props: {
-              bgcolor: $rgb(21, 126, 251)
-            },
-            layout: function(make, view) {
-              make.left.top.bottom.equalTo(0)
-              make.width.equalTo(view.super).multipliedBy(percentage)
-            }
+      views: [
+        {
+          type: "label",
+          props: {
+            text: (percentage * 100).toFixed(2) + "%",
+            font: $font("bold", 24)
+          },
+          layout: function(make, view) {
+            make.right.equalTo(0)
+            make.centerY.equalTo(view.super)
           }
-        ]
+        },
+        {
+          type: "view",
+          props: {
+            bgcolor: $rgba(21, 126, 251, 0.2),
+            radius: 8
+          },
+          layout: function(make, view) {
+            make.left.equalTo(0)
+            make.height.equalTo(32)
+            make.centerY.equalTo(view.super)
+            make.right.equalTo($("label").left).offset(-10)
+          },
+          views: [
+            {
+              type: "view",
+              props: {
+                bgcolor: $rgb(21, 126, 251)
+              },
+              layout: function(make, view) {
+                make.left.top.bottom.equalTo(0)
+                make.width.equalTo(view.super).multipliedBy(percentage)
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      type: "label",
+      props: {
+        id: "quote-label",
+        align: $align.center,
+        lines: 2,
+        textColor: $color("gray"),
+        font: $objc("UIFont").invoke("italicSystemFontOfSize", 15).rawValue()
+      },
+      layout: function(make, view) {
+        make.left.bottom.right.inset(10)
+      },
+      events: {
+        tapped: refreshQuote
       }
-    ]
-  }]
+    }
+  ]
 })
+
+function refreshQuote() {
+  $http.get({
+    url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json",
+    handler: function(resp) {
+      var label = $("quote-label").runtimeValue()
+      label.invoke("fadeToText", resp.data.quoteText)
+    }
+  })
+}
+
+refreshQuote()
