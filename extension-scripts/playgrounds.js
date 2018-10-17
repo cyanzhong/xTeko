bd3b83c41f10a018af1c39bac89b28d0e7c3cd8c
cyan <0x00eeee@gmail.com>
2018-10-17 21:59:06 +0800

    Update playgrounds.js

--- extension-scripts/playgrounds.js
+++ extension-scripts/playgrounds.js
@@ -1,4 +1,4 @@
 let string = await $qrcode.scan();
 let links = $detector.link(string);
-let {script} = await $http.get(links[0]);
-$addin.eval(script);
+let {data} = await $http.get(links[0]);
+$addin.eval(data);
