e4cdaa3ae5aeb9427aca274bb47ecb832e3044b2
cyan <0x00eeee@gmail.com>
2019-02-09 00:44:44 +0800

    Update painting.js

--- extension-scripts/painting.js
+++ extension-scripts/painting.js
@@ -1,19 +1,71 @@
+const __VERSION__ = 1.2;
+
+$objc("NSBundle").$bundleWithPath("/System/Library/PrivateFrameworks/MarkupUI.framework").$load();
+
+(async() => {
+  let url = "https://xteko.com/store/fetch?id=124";
+  let {data} = await $http.get(url);
+  let version = data.extension.version;
+
+  if (version === __VERSION__) {
+    return;
+  }
+
+  let actions = [$l10n("UPDATE"), $l10n("CANCEL")];
+  let {index} = await $ui.alert({
+    title: $l10n("FOUND_NEW_VERSION"),
+    message: $l10n("UPDATE_TO_NEW_VERSION"),
+    actions: actions
+  });
+
+  if (index !== 0) {
+    return;
+  }
+
+  let pkgURL = data.extension.url;
+  let pkgName = $addin.current.name;
+  let redirURL = `jsbox://import?url=${encodeURIComponent(pkgURL)}&name=${encodeURIComponent(pkgName)}`;
+
+  $app.openURL(redirURL);
+  canvasVC.$closeButtonTapped();
+})();
+
 $app.strings = {
   "en": {
     "UPGRADE_IOS": "Sorry, please upgrade your iOS.",
-    "TITLE": "Painting",
+    "UPDATE": "Update",
+    "FOUND_NEW_VERSION": "Found new version",
+    "UPDATE_TO_NEW_VERSION": "Update to the latest version?",
+    "DONE": "Done",
+    "CANCEL": "Cancel",
+    "CLEAR": "Clear",
+    "SETTING": "Setting",
     "COPY": "Copy",
     "SHARE": "Share",
-    "CLEAR": "Clear",
     "COPIED": "Copied",
+    "CANVAS_SIZE": "Canvas Size",
+    "FULL_SCREEN": "Full",
+    "A4_PAPER": "A4",
+    "SQUARE": "Square",
+    "CLEAR_AFTER_COPYING": "Clear After Copying",
   },
   "zh-Hans": {
     "UPGRADE_IOS": "抱歉，请升级你的 iOS 版本。",
-    "TITLE": "画图板",
+    "UPDATE": "更新",
+    "FOUND_NEW_VERSION": "发现新版本",
+    "UPDATE_TO_NEW_VERSION": "更新到最新版本？",
+    "DONE": "完成",
+    "CANCEL": "取消",
+    "CLEAR": "清除",
+    "SETTING": "设置",
     "COPY": "复制",
     "SHARE": "分享",
-    "CLEAR": "清除",
     "COPIED": "已复制",
+    "CANVAS_SIZE": "画布大小",
+    "FULL_SCREEN": "全屏",
+    "A4_PAPER": "A4 纸",
+    "SQUARE": "方形",
+    "CLEAR_AFTER_COPYING": "复制后清除",
   }
 };
 
@@ -22,8 +74,6 @@ if (majorVersion < 12) {
   alert($l10n("UPGRADE_IOS"));
   return;
 }
-
-$objc("NSBundle").$bundleWithPath("/System/Library/PrivateFrameworks/MarkupUI.framework").$load();
 
 let lowMemory = $app.env != $env.app;
 let widgetHeight = $widget.height;
@@ -103,12 +153,20 @@ $define({
 });
 
 $define({
+  type: "PKNavigator: UINavigationController",
+  events: {
+    "supportedInterfaceOrientations": () => {
+      return 1 << 1;
+    }
+  }
+});
+
+$define({
   type: "PKCanvasVC: MarkupViewController",
-  props: ["canvas"],
+  props: ["canvas", "topLine", "bottomLine"],
   events: {
     "viewDidLoad": () => {
       self.$super().$viewDidLoad();
-      self.$navigationItem().$setTitle($l10n("TITLE"));
       self.$setBackgroundColor($color("white").runtimeValue());
       self.$setShowShareButtonInToolbar(true);
       self.$didReceiveMemoryWarning(null);
@@ -119,6 +177,17 @@ $define({
       self.$setCanvas(canvas);
       self.$view().$addSubview(canvas);
       self.$setImage($objc("UIImage").$new());
+
+      let lineColor = $color("lightGray").runtimeValue();
+      let topLine = $objc("UIView").$new();
+      topLine.$setBackgroundColor(lineColor);
+      self.$setTopLine(topLine);
+      self.$view().$addSubview(topLine);
+
+      let bottomLine = $objc("UIView").$new();
+      bottomLine.$setBackgroundColor(lineColor);
+      self.$setBottomLine(bottomLine);
+      self.$view().$addSubview(bottomLine);
 
       let toolbar = self.$modernToolbar();
       toolbar.$setCanvas(canvas);
@@ -139,32 +208,68 @@ $define({
         toolbar.$valueForKey(key).$setTintColor(tintColor);
       });
 
-      let doneButton = $objc("UIBarButtonItem").$alloc().$initWithBarButtonSystemItem_target_action(0, self, "closeButtonTapped");
+      let doneButton = $objc("UIBarButtonItem").$alloc().$initWithTitle_style_target_action($l10n("DONE"), 0, self, "closeButtonTapped");
       let clearButton = $objc("UIBarButtonItem").$alloc().$initWithTitle_style_target_action($l10n("CLEAR"), 0, self, "clearButtonTapped");
+      let settingButton = $objc("UIBarButtonItem").$alloc().$initWithTitle_style_target_action($l10n("SETTING"), 0, self, "settingButtonTapped");
+
       let navButtons = NSMutableArray.$new();
       navButtons.$addObject(doneButton);
       navButtons.$addObject(clearButton);
+
+      if (!lowMemory) {
+        navButtons.$addObject(settingButton);
+      }
+      
       self.$navigationItem().$setLeftBarButtonItems(navButtons);
     },
     "viewDidLayoutSubviews": () => {
       self.$super().$viewDidLayoutSubviews();
+
       let canvas = self.$canvas();
       let maxBounds = self.$view().$bounds();
       let toolbarBounds = self.$modernToolbar().$bounds();
+      let pageHeight = maxBounds.height - toolbarBounds.height;
+
+      let canvasWidth = maxBounds.width;
+      let canvasHeight = (() => {
+        let pageSize = $cache.get("page-size");
+        switch (pageSize) {
+          case 0: return pageHeight;
+          case 1: return Math.min(pageHeight, maxBounds.width * 1.4142);
+          case 2: return maxBounds.width;
+        }
+      })();
+
       canvas.$setFrame({
         "x": 0,
-        "y": 0,
-        "width": maxBounds.width,
-        "height": maxBounds.height - toolbarBounds.height
+        "y": (pageHeight - canvasHeight) * 0.5,
+        "width": canvasWidth,
+        "height": canvasHeight
+      });
+
+      let canvasFrame = canvas.$frame();
+      let lineHeight = 1.0 / $device.info.screen.scale;
+
+      self.$topLine().$setFrame({
+        "x": 0,
+        "y": canvasFrame.y,
+        "width": canvasFrame.width,
+        "height": lineHeight
+      });
+
+      self.$bottomLine().$setFrame({
+        "x": 0,
+        "y": canvasFrame.y + canvasFrame.height,
+        "width": canvasFrame.width,
+        "height": lineHeight
       });
     },
     "closeButtonTapped": () => {
       self.$dismissViewControllerAnimated_completion(true, null);
       $widget.height = widgetHeight;
     },
-    "clearButtonTapped": () => {
-      self.$canvas().$eraseAll();
-    },
+    "clearButtonTapped": () => self.$canvas().$eraseAll(),
+    "settingButtonTapped": () => settingButtonTapped(),
     "_toolbarShareButtonTapped:": () => {
 
       let canvas = self.$canvas();
@@ -172,8 +277,8 @@ $define({
       let size = {"width": bounds.width, "height": bounds.height};
       let scale = lowMemory ? 1 : $device.info.screen.scale;
       let renderer = $objc("PKImageRenderer").$alloc().$initWithSize_scale_useMetal(size, scale, true);
-
       let drawing = canvas.$drawing();
+
       let block = $block("void, UIImage *", image => {
         
         $thread.main({
@@ -184,6 +289,10 @@ $define({
               $clipboard.image = image.rawValue();
               $device.taptic(2);
               $ui.toast($l10n("COPIED"));
+
+              if ($cache.get("auto-clear")) {
+                canvasVC.$clearButtonTapped();
+              }
             }
 
             function shareImage() {
@@ -194,7 +303,7 @@ $define({
               copyImage();
             } else {
               let {index} = await $ui.menu([$l10n("COPY"), $l10n("SHARE")]);
-              if (index == 0) {
+              if (index === 0) {
                 copyImage();
               } else {
                 shareImage();
@@ -214,6 +323,97 @@ $define({
   }
 });
 
+if (!lowMemory) {
+  $define({
+    type: "PKSettingVC: UIViewController<UITableViewDataSource, UITableViewDelegate>",
+    props: ["cells", "tableView", "completionBlock"],
+    events: {
+      "viewDidLoad": () => {
+        self.$super().$viewDidLoad();
+        self.$view().$setBackgroundColor($color("white").runtimeValue());
+  
+        let sizeCell = (() => {
+  
+          let cell = $objc("UITableViewCell").$alloc().$initWithStyle_reuseIdentifier(0, "sizeCell");
+          cell.$textLabel().$setText($l10n("CANVAS_SIZE"));
+  
+          let items = NSMutableArray.$new();
+          items.$addObject($l10n("FULL_SCREEN"));
+          items.$addObject($l10n("A4_PAPER"));
+          items.$addObject($l10n("SQUARE"));
+  
+          let control = $objc("UISegmentedControl").$alloc().$initWithItems(items);
+          control.$setTintColor($color("tint").runtimeValue());
+          control.$setSelectedSegmentIndex($cache.get("page-size") || 0);
+          control.$addTarget_action_forControlEvents(self, "pageSizeChanged:", 1 << 12);
+          cell.$setAccessoryView(control);
+  
+          return cell;
+        })();
+  
+        let clearCell = (() => {
+  
+          let cell = $objc("UITableViewCell").$alloc().$initWithStyle_reuseIdentifier(0, "clearCell");
+          cell.$textLabel().$setText($l10n("CLEAR_AFTER_COPYING"));
+  
+          let switcher = $objc("UISwitch").$new();
+          switcher.$setOnTintColor($color("tint").runtimeValue());
+          switcher.$setOn($cache.get("auto-clear") || false);
+          switcher.$addTarget_action_forControlEvents(self, "clearBehaviorChanged:", 1 << 12);
+          cell.$setAccessoryView(switcher);
+  
+          return cell;
+        })();
+  
+        let cells = NSMutableArray.$new();
+        cells.$addObject(sizeCell);
+        cells.$addObject(clearCell);
+        self.$setCells(cells);
+  
+        let tableView = $objc("UITableView").$alloc().$initWithFrame_style({
+          "x": 0,
+          "y": 0,
+          "width": 0,
+          "height": 0
+        }, 1);
+  
+        tableView.$setAllowsSelection(false);
+        tableView.$setDataSource(self);
+        tableView.$setDelegate(self);
+  
+        self.$setTableView(tableView);
+        self.$view().$addSubview(tableView);
+      },
+      "viewDidLayoutSubviews": () => {
+        self.$super().$viewDidLayoutSubviews();
+  
+        let frame = self.$view().$bounds();
+        self.$tableView().$setFrame(frame);
+      },
+      "tableView:numberOfRowsInSection:": (tableView, section) => {
+        return self.$cells().$count();
+      },
+      "tableView:cellForRowAtIndexPath:": (tableView, indexPath) => {
+        return self.$cells().$objectAtIndex(indexPath.$row());
+      },
+      "tableView:didSelectRowAtIndexPath:": (tableView, indexPath) => {
+        tableView.$deselectRowAtIndexPath_animated(indexPath, true);
+      },
+      "pageSizeChanged": sender => {
+        let index = sender.$selectedSegmentIndex();
+        $cache.set("page-size", index);
+        self.$notifyChanges();
+      },
+      "clearBehaviorChanged": sender => {
+        let on = sender.$isOn();
+        $cache.set("auto-clear", on);
+        self.$notifyChanges();
+      },
+      "notifyChanges": () => self.$completionBlock()()
+    }
+  });
+}
+
 function getInk() {
   let _ink = $cache.get("ink");
   if (_ink) {
@@ -227,7 +427,17 @@ function getInk() {
   }
 }
 
+function settingButtonTapped() {
+  let settingVC = $objc("PKSettingVC").$new();
+  settingVC.$setCompletionBlock(() => {
+    let view = canvasVC.$view();
+    view.$setNeedsLayout();
+    view.$layoutIfNeeded();
+  });
+  navigator.$pushViewController_animated(settingVC, true);
+}
+
 let canvasVC = $objc("PKCanvasVC").$new();
-let navigator = $objc("UINavigationController").$alloc().$initWithRootViewController(canvasVC);
+let navigator = $objc("PKNavigator").$alloc().$initWithRootViewController(canvasVC);
 let rootVC = $ui.controller.runtimeValue();
 rootVC.$presentViewController_animated_completion(navigator, true, null);
