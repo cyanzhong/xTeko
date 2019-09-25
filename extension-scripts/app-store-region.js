dd21ec59bfe3d6a7bda59578593c6d4742428745
cyan <0x00eeee@gmail.com>
2019-09-25 22:15:53 +0800

    Update app-store-region.js

--- extension-scripts/app-store-region.js
+++ extension-scripts/app-store-region.js
@@ -1,20 +1,19 @@
-var regions = [
+let regions = [
   { name: "🇨🇳 中国", code: "cn" },
   { name: "🇭🇰 香港", code: "hk" },
-  { name: "🇨🇳 台湾", code: "tw" },
   { name: "🇺🇸 美国", code: "us" },
   { name: "🇬🇧 英国", code: "uk" },
   { name: "🇯🇵 日本", code: "jp" },
 ]
 
 let version = parseInt($device.info.version.split(".")[0]);
-let domain = version >= 13 ? "itms://apps.apple.com/" : "https://itunes.apple.com/";
+let prefix = version >= 13 ? "itms-apps://apps.apple.com/" : "https://itunes.apple.com/";
 
 $ui.menu({
   items: regions.map(function(item) {
     return item.name;
   }),
   handler: function(title, idx) {
-    $app.openURL(domain + regions[idx].code + "/app/");
+    $app.openURL(prefix + regions[idx].code + "/app/");
   }
 });
