68f2abba2231ef1747656b56041ffb93b9963a80
cyan <0x00eeee@gmail.com>
2020-12-28 17:06:29 +0800

    Fix MarkMe

--- extension-scripts/markme/scripts/settings/theme.js
+++ extension-scripts/markme/scripts/settings/theme.js
@@ -1,21 +1,99 @@
 const storage = require("./storage");
 const helper = require("./helper");
 const preview = require("./preview");
-const util = require("../common/util");
 
 exports.open = callback => {
-  const bundle = $objc("NSBundle").$mainBundle();
-  const path = bundle.$bundlePath().$stringByAppendingPathComponent("Styles");
-  const manager = $objc("NSFileManager").$defaultManager();
-  const files = manager.$contentsOfDirectoryAtPath_error(path, null);
-  const themes = [];
-
-  for (let idx=0; idx<files.$count(); ++idx) {
-    const theme = files.$objectAtIndex(idx).rawValue();
-    themes.push(util.removePathExtension(theme));
-  }
-
-  const data = themes.sort();
+  const data = [
+    "atelier-estuary-dark",
+    "a11y-dark",
+    "a11y-light",
+    "agate",
+    "an-old-hope",
+    "androidstudio",
+    "arduino-light",
+    "arta",
+    "ascetic",
+    "atelier-cave-dark",
+    "atelier-cave-light",
+    "atelier-dune-dark",
+    "atelier-dune-light",
+    "atelier-estuary-light",
+    "atelier-forest-dark",
+    "atelier-forest-light",
+    "atelier-heath-dark",
+    "atelier-heath-light",
+    "atelier-lakeside-dark",
+    "atelier-lakeside-light",
+    "atelier-plateau-dark",
+    "atelier-plateau-light",
+    "atelier-savanna-dark",
+    "atelier-savanna-light",
+    "atelier-seaside-dark",
+    "atelier-seaside-light",
+    "atelier-sulphurpool-dark",
+    "atelier-sulphurpool-light",
+    "atom-one-dark",
+    "atom-one-light",
+    "codepen-embed",
+    "color-brewer",
+    "darcula",
+    "dark",
+    "default",
+    "docco",
+    "dracula",
+    "far",
+    "foundation",
+    "github-gist",
+    "github",
+    "gml",
+    "googlecode",
+    "grayscale",
+    "gruvbox-dark",
+    "gruvbox-light",
+    "hopscotch",
+    "hybrid",
+    "idea",
+    "ir-black",
+    "isbl-editor-dark",
+    "isbl-editor-light",
+    "kimbie.dark",
+    "kimbie.light",
+    "lightfair",
+    "lioshi",
+    "magula",
+    "mono-blue",
+    "monokai-sublime",
+    "monokai",
+    "night-owl",
+    "nord",
+    "obsidian",
+    "ocean",
+    "paraiso-dark",
+    "paraiso-light",
+    "purebasic",
+    "qtcreator_dark",
+    "qtcreator_light",
+    "railscasts",
+    "rainbow",
+    "routeros",
+    "shades-of-purple",
+    "solarized-dark",
+    "solarized-light",
+    "srcery",
+    "stackoverflow-dark",
+    "stackoverflow-light",
+    "sunburst",
+    "tomorrow-night-blue",
+    "tomorrow-night-bright",
+    "tomorrow-night-eighties",
+    "tomorrow-night",
+    "tomorrow",
+    "vs",
+    "vs2015",
+    "xcode",
+    "xt256",
+    "zenburn",
+  ];
   const selectedIndex = data.indexOf(storage.theme());
 
   $ui.push({
